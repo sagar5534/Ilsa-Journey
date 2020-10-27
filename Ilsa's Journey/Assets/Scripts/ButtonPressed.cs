96f4fdad67ed12b5f2dc5746a0e700d2c46c2fb7
Sagar P <patelsag@pointclickcare.com>
2020-10-26 22:39:14 -0400

    PreAnimation

---
+++ Ilsa's Journey/Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressed : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        anim = GameObject.Find("RotatingGO").GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        GameObject player = GameObject.Find("Player");
+        Vector3 playFloor = player.transform.position + Vector3.down;
+
+        if (playFloor == transform.position){
+            anim.enabled = true;
+        }     
+
+    }
+
+}
+
