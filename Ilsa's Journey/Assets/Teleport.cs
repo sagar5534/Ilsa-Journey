3d6c840f24dc89b12bfd9bce9dd0b439eb6429b8
Sagar <patelsag@pointclickcare.com>
2020-12-07 01:58:22 -0500

    Adding map 2 fixes and features

---
+++ Ilsa's Journey/Assets/Teleport.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public Vector3 location;
+	public GameObject button;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+					
+        	Vector3 playFloor = button.transform.position + Vector3.up;
+			print(transform.position);
+        	if (playFloor == transform.position){
+				print("Made it");
+				transform.position = location;
+				print(transform.position);
+				
+				
+				MoveCharacter script = GetComponent("MoveCharacter") as MoveCharacter;
+				GetComponent<MoveCharacter>().Init();
+				
+				
+				//script.StandingFloor = button;
+				
+				enabled = false;
+        	}     
+		
+    }
+}
